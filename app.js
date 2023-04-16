31dd611d788d136729afef75761a5da37e19cb0f
cast <mc1@email.com>
2023-04-16 12:24:21 -0700

    new changes

--- app.js
+++ app.js
@@ -1,56 +1,57 @@
 const express = require("express");
+const app = express();
 const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
+app.use(express.json());
+const cors = require("cors");
+app.use(cors());
 
 const mongoUrl =
   "mongodb+srv://fsdi456:SANdiego456@wakeuppal.lgugkqe.mongodb.net/wakeuppal?retryWrites=true&w=majority";
-
-const app = express();
-app.use(bodyParser.json());
-
-// connect to DB
 mongoose
   .connect(mongoUrl, {
     useNewUrlParser: true,
   })
   .then(() => {
-    console.log("Connected to database");
+    console.log("connected to database");
   })
-  .catch((e) => console.error(e));
+  .catch((e) => console.log(e));
 
-mongoose.Promise = global.Promise;
+const User = require("./userDetailsSchema");
 
-const alarmSchema = new mongoose.Schema({
-  userName: String,
-  alermname: String,
-  time: Number,
-  ring: String,
-});
-const Alarm = mongoose.model("alarms", alarmSchema);
+//const User = mongoose.model("Userinfo");
 
-// API ENDPOINTS
-
-app.get("/alarms", (req, res) => {
-  Alarm.find()
-    .then((result) => res.send(result))
-    .catch((err) => res.status(500).send({ message: err.message }));
+app.get("/", async(req, res) => {
+  res.send("Welcome page here in the future");
 });
 
-app.post("/alarms", async (req, res) => {
-  const alarm = new Alarm(req.body);
-  alarm
-    .save()
-    .then(() => res.send(alarm))
-    .catch((err) => res.status(500).send({ message: err.message }));
+app.get("/users", async (req, res) => {
+  const user = await User.find();
+  res.send(user);
 });
 
-// START server
+app.post("/register", async (req, res) => {
+  const { fname, lname, email, password } = req.body;
+  try {
+    const oldUser = await User.findOne({ email });
+    console.log(email);
+    console.log(oldUser);
+    if (oldUser) {
+      res.send({ error: "User Exists" });
+    } else {
+      await User.create({
+        fname,
+        lname,
+        email,
+        password,
+      });
+      res.send({ status: "ok" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.send({ status: "error" });
+  }
+});
 
-/**
- * run: node app.js
- * REMEMBER TO STOP AND START THE SERVER WHEN THE CODE CHANGES
- *
- */
 app.listen(5000, () => {
   console.log("Server Started");
 });
